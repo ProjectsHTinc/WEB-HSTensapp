479b361801fe4f6610150943a86aa5189b3f5472
kamalhappysanz <kamal.happysanz@gmail.com>
2019-11-04 15:50:34 +0530

    Update Metrics.php

--- application/controllers/Metrics.php
+++ application/controllers/Metrics.php
@@ -33,10 +33,30 @@ class Metrics extends CI_Controller {
 
 
 	public function list_institute(){
+		// $url = $this->url;
+		// $data['objectlist'] = file_get_contents($url);
+		// $str = $data['objectlist'];
+		// echo stripslashes($str);
 		$url = $this->url;
-		$data['objectlist'] = file_get_contents($url);
-		$str = $data['objectlist'];
-		echo stripslashes($str);
+		$ch = curl_init();
+		curl_setopt ($ch, CURLOPT_URL, $url);
+		curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, 5);
+		curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);
+		$contents = curl_exec($ch);
+		if (curl_errno($ch)) {
+		  echo curl_error($ch);
+		  echo "\n<br />";
+		  $contents = '';
+		} else {
+		  curl_close($ch);
+		}
+
+		if (!is_string($contents) || !strlen($contents)) {
+		echo "Failed to get contents.";
+		$contents = '';
+		}
+
+		echo $contents;
 	}
 
 
